79fcadcc868b3128eb9dc0077bcf88d4e79b7e01
alina1412 <alina1412@mail.ru>
2022-11-08 11:53:58 +0300

    add actions

--- weather_app/config/config.py
+++ weather_app/config/config.py
@@ -1,6 +1,6 @@
-import os
+from os import environ 
 
 
 class Settings:
-    openweather_api_key = os.environ.get("weather_api_key")
-    app_key = os.environ.get("app_key")
+    openweather_api_key = environ.get("weather_api_key")
+    app_key = environ.get("app_key")
